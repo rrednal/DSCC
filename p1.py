8662f40aafc1b8ff93a78862f6fe1a173da1c6aa
rrednal <84549672+rrednal@users.noreply.github.com>
2023-12-12 09:13:48 +0300

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,42 +1,37 @@
 import psycopg2
-import socket
-from threading import Thread
+from flask import Flask, request, jsonify
+import logging
 
-conn = psycopg2.connect(database="DB", user="myLogin", password="databasePass123", host="localhost", port=5432)
+app = Flask(__name__)
 
+conn = psycopg2.connect(database="postgres", user="postgres", password="password", host="localhost", port=5432)
 cur = conn.cursor()
-sock1 = socket.socket()
-sock1.bind(('127.0.0.1', 5432))
-k=1
-sock1.listen(1)
-cursor = sock1.accept()
-cursor.execute('SELECT num FROM table1')
-database = cursor.fetchall()
+cur.execute('SELECT num FROM table1')
+database = [row[0] for row in cur.fetchall()]
 
+@app.route("/", methods=["POST"])
 def function():
-    err = 0
-    while True:
-        a = int(conn.recv(1024))+1
-        if a in database:
-            msg="В БД обнаружено совпадение. Попробуйте еще раз".encode("utf-8")
-            insert_q = """INSERT INTO table1 (log) VALUES 'ошибка 1'"""
-            cursor.execute()
+    try:
+        data = request.get_json()
+        a = int(data.get('number'))
+    except (ValueError, TypeError):
+        return jsonify({"error": "Incorrect data"})
+    if a in database:
+        msg="Error 1. Match in DB"
+        logging.error(msg)
+    else:
+        if (a + 1) in database:
+            msg="Error 2. Number is less by 1 than the processed one"
+            logging.error(msg)
         else:
-            if a == database[k-1]:
-                msg="Число меньше предыдущего на 1".encode("utf-8")
-                insert_q = """INSERT INTO table1 (log) VALUES 'ошибка 2'"""
-                cursor.execute()
-                conn.commit()
-            else:
-                database.append(a)
-                msg = str(a).encode("utf-8")
-                insert_q = """INSERT INTO table1 (num) VALUES (%s)"""
-                rec = (a)
-                cursor.execute(insert_q, rec)
+            database.append(a)
+            msg = str(a + 1)
+            insert_q = """INSERT INTO table1 (num) VALUES (%s)"""
+            rec = (a, )
+            cur.execute(insert_q, rec)
             conn.commit()
-            count = cursor.rowcount
-        k=k+1
-        conn.send(msg)
+    return jsonify({"result": msg})
      
-t = Thread(target=function)
-t.start()
+if __name__ == '__main__':
+    logging.basicConfig(filename='app.log', level=logging.ERROR)
+    app.run(host='127.0.0.1', port=5000)
